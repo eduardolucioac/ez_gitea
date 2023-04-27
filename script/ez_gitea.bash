5b4a689d42935bfac0819e6b5014c018c7e8975b
Eduardo Lúcio Amorim Costa <eduardolucioac@gmail.com>
2023-04-27 20:41:52 -0300

    Update ez_gitea.bash

--- script/ez_gitea.bash
+++ script/ez_gitea.bash
@@ -6,6 +6,6 @@ cd "$SCRIPT_DIR"
 
 docker-compose -f ../docker-compose/docker-compose.yml --env-file ../SETTINGS up -d
 sleep 2
-google-chrome-stable --user-data-dir="/tmp" --app="http://0.0.0.0:3000/" & CHROME_PID=$!
+google-chrome-stable --user-data-dir="/tmp" --app="http://localhost:3000/" & CHROME_PID=$!
 wait $CHROME_PID
 docker-compose -f ../docker-compose/docker-compose.yml --env-file ../SETTINGS stop
